7f29f3b962d5ab4ebb676e8f8babc74925ab11bd
Ushnish22 <ushnish1997>
2023-02-09 15:40:53 +0530

    New_Views_JS

--- file.js
+++ file.js
@@ -1,3 +1,5 @@
+const path=require('path');
+
 const http=require('http');
 
 const express=require('express');
@@ -6,16 +8,20 @@ const bodyParser=require('body-parser');
 
 const app=express();
 
-const loginRoutes=require('./routes/chatapp/login');
-const messageRoutes=require('./routes/chatapp/message');
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+const contactRoutes=require('./routes/contactus');
+const successRoutes=require('./routes/success');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(loginRoutes);
-app.use(messageRoutes);
+app.use('/admin', adminRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
+app.use(shopRoutes);
 
-/*app.use((req,res,next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
-});*/
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(5000);
